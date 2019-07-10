68787ab7006b4ecd6a92defa2a70850d949fdcdc
aswanth1989 <aswanthlearning@gmail.com>
2019-07-11 01:05:39 +0800

    Adding hooks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Person from './Person/Person';
 import './App.css';
 
 function App() {
+  const [personState,setPersonState] = useState({
+    persons:[
+      {name:'Aswa', age:29},
+      {name:'Neetha', age:29},
+      {name:'Arun', age:33}
+    ],
+    otherState:'Some other value'
+  });
+
+  const switchHandler = ()=> {
+    setPersonState({
+      persons:[
+        {name:'Aswanth', age:29},
+        {name:'Neetha', age:29},
+        {name:'Arun', age:33}
+      ],
+      otherState:'Some other value'
+    });
+  };
+
   return (
     <div className="App">
       <h1>This is React App</h1>
-      <Person name="Aswa" age="29"/>
-      <Person name="Neetha" age="29">Hobbies: Racing</Person>
+      <button onClick={switchHandler}>Switch Name</button>
+      <Person name={personState.persons[0].name} age={personState.persons[0].age}/>
+      <Person name={personState.persons[1].name} age={personState.persons[1].age}>Hobbies: Racing</Person>
+      <Person name={personState.persons[2].name} age={personState.persons[2].age}/>
     </div>
   );
 }
