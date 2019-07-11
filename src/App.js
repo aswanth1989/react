50a1cfd5fc9b65436e294108417addcbc4208f4f
aswanth1989 <aswanthlearning@gmail.com>
2019-07-12 00:39:44 +0800

    Adding two way binding

--- src/App.js
+++ src/App.js
@@ -12,11 +12,22 @@ function App() {
     otherState:'Some other value'
   });
 
-  const switchHandler = ()=> {
+  const switchHandler = (newName)=> {
     setPersonState({
       persons:[
-        {name:'Aswanth', age:29},
+        {name:newName, age:29},
         {name:'Neetha', age:29},
+        {name:'Arun', age:33}
+      ],
+      otherState:'Some other value'
+    });
+  };
+
+  const namechangedHandler = (event) =>{
+    setPersonState({
+      persons:[
+        {name:'Aswa', age:29},
+        {name: event.target.value, age:29},
         {name:'Arun', age:33}
       ],
       otherState:'Some other value'
@@ -26,10 +37,18 @@ function App() {
   return (
     <div className="App">
       <h1>This is React App</h1>
-      <button onClick={switchHandler}>Switch Name</button>
-      <Person name={personState.persons[0].name} age={personState.persons[0].age}/>
-      <Person name={personState.persons[1].name} age={personState.persons[1].age}>Hobbies: Racing</Person>
-      <Person name={personState.persons[2].name} age={personState.persons[2].age}/>
+      <button onClick={switchHandler.bind(this,'Aswanth M')}>Switch Name</button>
+      <Person 
+        name={personState.persons[0].name} 
+        age={personState.persons[0].age}/>
+      <Person 
+        name={personState.persons[1].name} 
+        age={personState.persons[1].age}
+        click={switchHandler.bind(this,'Aswanth Muthukrishnan')}
+        changed={namechangedHandler}>Hobbies: Racing</Person>
+      <Person 
+        name={personState.persons[2].name} 
+        age={personState.persons[2].age}/>
     </div>
   );
 }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,8 +4,9 @@ const person = (props)=> {
 
     return (
     <div>
-        <p>I am {props.name} and I am {props.age}!</p>
+        <p onClick={props.click}>I am {props.name} and I am {props.age}!</p>
         <p>{props.children}</p>
+        <input type="text" onChange={props.changed}/>
     </div>
     )
 }
